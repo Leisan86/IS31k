f198cbca82db67451688d867615a4c84d7293cda
Un Couteau <5453712-un_couteau@users.noreply.gitlab.com>
2022-11-24 23:31:02 +0500

    upgrades / bug fixes

--- switcher.py
+++ switcher.py
@@ -1,11 +1,11 @@
 import os
+from typing import NoReturn
 
-if __name__ == "__main__":
+
+def main() -> NoReturn:
     choice = int(input("Введите вариант работы(1-18): "))
 
-    lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
-
-    if choice in lst:
+    if choice in range(1, 19):
         if os.system(f"git switch {choice}") == 0:
             print(f"\033[33mИдет выполнение работы\033[0m \033[31m{choice}\033[33m варианта\033[0m")
             if os.path.exists("main.py"):
@@ -16,3 +16,7 @@ if __name__ == "__main__":
             print("\033[31mОшибка!\033[0m\nВетка не была найдена. Попробуйте обновить командой \033[33mgit pull\033[0m")
     else:
         print("\033[31mОшибка!\033[0m\nДоступные значение с 1 до 18")
+
+
+if __name__ == "__main__":
+    main()

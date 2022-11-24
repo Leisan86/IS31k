18e57b0f8c95ee777f924d3d8b1fa30b8bff2694
lia-gat <114720640+lia-gat@users.noreply.github.com>
2022-11-24 14:12:12 +0500

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,45 @@
+import unittest
+from Calc import Calc
+
+class CalcTestCase(unittest.TestCase):
+    def test_disc(self):
+        a = 3
+        b = 10
+        c = 2
+        u = Calc(a, b, c)
+        res = u.disc()
+        self.assertEqual(res, 76)
+
+    def test_roots(self):
+        a = 3
+        b = -12
+        c = 0
+        u = Calc(a, b, c)
+        u.disc()
+        res = u.root()
+        self.assertEqual(res, None)
+
+    def test_zero_div(self):
+        a = 0
+        b = 0
+        c = 10
+        u = Calc(a, b, c)
+        u.disc()
+        res = u.root()
+        self.assertEqual(res, 'Введите корректные значения')
+
+    def test_root(self):
+        a = 9
+        b = 0
+        c = 0
+        u = Calc(a, b, c)
+        u.disc()
+        res = u.root()
+        self.assertEqual(res, None)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
